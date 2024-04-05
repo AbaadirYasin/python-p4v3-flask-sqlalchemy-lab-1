0c640bf70e43ac51a65893f085c24a803f13da77
AbaadirYasin <abaadir.yasin@student.moringaschool.com>
2024-04-05 14:34:29 +0100

    completed

--- server/app.py
+++ server/app.py
@@ -1,53 +1,40 @@
-# server/app.py
-#!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
 @app.route('/earthquakes/<int:id>')
-def earthquakes(id):
-    earthquake =Earthquake.query.filter(Earthquake.id == id).first()
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
     if not earthquake:
-      body= {'message': f'Earthquake {id} not found.'}
-      status= 404
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
     else:
-      body = earthquake.to_dict()
-      status= 200
+        body = earthquake.to_dict()
+        status = 200
             
     return make_response(body, status)
 
 @app.route('/earthquakes/magnitude/<float:magnitude>')
-def min_magnitude(magnitude):
-    quakes=[]
-    for earthquakes in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
-        quakes.append(earthquakes.to_dict())
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
     if quakes:
-        body={'count':len(quakes),
-               'quakes': quakes}
-                    
+        body = {'count': len(quakes), 'quakes': [quake.to_dict() for quake in quakes]}
     else:
-        body={'count':0,
-               'quakes': quakes}
+        body = {'count': 0, 'quakes': []}
         
-    return make_response(body,200)
-  
-    return make_response(body, status)
+    return make_response(body, 200)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
